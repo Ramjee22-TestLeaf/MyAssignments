12af2d2b6d0b11cade8fea7563b428f631c134af
Ramjee22-TestLeaf <durgashwanth@gmail.com>
2025-07-13 16:28:41 +0530

    Add files via upload
    week1.day1 My First Program

---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+//Access modifier(public) Keyword(class) and className(FirstProgram)
+//Methods and variables (Properties of class)
+
+public class FirstProgram 
+{
+	
+//Declare main method inside the class
+	
+	public static void main(String args[])
+	{
+		System.out.println("My first Program");
+	}
+}
