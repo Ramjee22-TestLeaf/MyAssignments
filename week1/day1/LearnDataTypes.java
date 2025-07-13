ac7a25a7cbbe7c77485021844be22d9dac1f6850
Ramjee22-TestLeaf <durgashwanth@gmail.com>
2025-07-13 18:18:50 +0530

    Add files via upload
    Day 1 Home Assignment

---
+++ week1/day1/LearnDataTypes.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) 
+	{
+		byte age = 38;
+		short ageOfPerson=128;  // If more than 1 word use camel case
+		int  amount = 40000;
+		long mobileNumber = 9876543210l;
+		float roi=10.50F;
+		float currency=8.76543210f;
+		double rupees=8.76543210895;
+		char logo='T';
+		String name="Ramjee"; // String is a predefined class in JAVA
+		boolean doYouKnowJava=true;
+		System.out.println(rupees);
+		System.out.println(currency);
+        System.out.println(doYouKnowJava);
+        System.out.println("My name is: " + name);
+        System.out.println(name+" "+age);
+	}
+
+}
